b9a8eb630f4436200eb63bf61c361d4ee55f40a9
Tharindu Gamage <tharindu.kumara@visioinnovation.com>
2025-12-04 23:34:49 +0530

    Remove logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,14 +10,14 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="flex items-center gap-3">
-              <Image
+              {/* <Image
                 src="/logo.png"
                 alt="Help2Study Logo"
                 width={40}
                 height={40}
                 className="object-contain"
                 priority
-              />
+              /> */}
               <span className="text-xl font-bold text-indigo-600">Help2Study</span>
             </Link>
           </div>
